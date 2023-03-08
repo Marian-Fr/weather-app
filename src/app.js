5a8847bcaf906fdea5c3925615122900d00dfd73
Marian-Fr <126185488+Marian-Fr@users.noreply.github.com>
2023-03-08 18:07:13 +0100

    Fixed time and day

--- src/app.js
+++ src/app.js
@@ -1,19 +1,18 @@
-function formatDate(timestamp) {
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10);
-  hours= `${hours}`;
-  let minutes = date.getMinutes();
-  if (minutes < 10);
-  minutes= `${minutes}`;
-  
-  let days=["Sunday", "Monday", "Tuesday","Wednesday", "Thursday","Friday", "Saturday"];
-  let day = days[date.getDate()]; 
-  return `${day} ${hours}:${minutes}`;
+function formatDate(timestamp){
+  let date=new Date(timestamp)
+let hours= date.getHours();
+if(hours < 10)
+hours=`0${hours}`;
+let minutes=date.getMinutes();
+if(minutes < 10)
+minutes=`0${minutes}`;
+let days= ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+console.log(days)
+let day=days[date.getDay()];
+return `${day} ${hours}:${minutes}`
+
+
 }
-
-
-
 
 function displayTemperature(response){
     console.log(response.data)
@@ -28,15 +27,16 @@ function displayTemperature(response){
     let windElement=document.querySelector("#wind");
     windElement.innerHTML=Math.round(response.data.wind.speed)
      let dateElement=document.querySelector("#date");
-     dateElement.innerHTML=formatDate(response.data.time);
+     dateElement.innerHTML=formatDate(response.data.time *1000);
     ;
     let iconElement=document.querySelector("#icon")
-    iconElement.setAttribute("src", `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
+    iconElement.setAttribute("src" ,`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
     iconElement.setAttribute("alt",response.data.condition.description)
 
 }
     
 let apiKey="2e8483398ocdfb8e6befab4eb2at6740";
-let city="Kyiv";
+let city="Lisbon";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
 axios.get(apiUrl).then(displayTemperature);
